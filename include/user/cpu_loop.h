1c1180eab64f9ee752e26b365e3423226ab29d76
Aoto Tokizawa <aoto.tokizawa@gmail.com>
2025-04-05 05:07:20 -0700

    [JOVE] for helpers, cpu_loop_exit_sigsegv() just traps

--- include/user/cpu_loop.h
+++ include/user/cpu_loop.h
@@ -48,6 +48,18 @@ MMUAccessType adjust_signal_pc(uintptr_t *pc, bool is_write);
 bool handle_sigsegv_accerr_write(CPUState *cpu, sigset_t *old_set,
                                  uintptr_t host_pc, abi_ptr guest_addr);
 
+#ifdef CONFIG_JOVE_HELPERS
+
+inline
+G_NORETURN void cpu_loop_exit_sigsegv(CPUState *cpu, target_ulong addr,
+                                      MMUAccessType access_type,
+                                      bool maperr, uintptr_t ra) {
+    __builtin_trap();
+    __builtin_unreachable();
+}
+
+#else
+
 /**
  * cpu_loop_exit_sigsegv:
  * @cpu: the cpu context
@@ -62,6 +74,8 @@ bool handle_sigsegv_accerr_write(CPUState *cpu, sigset_t *old_set,
 G_NORETURN void cpu_loop_exit_sigsegv(CPUState *cpu, target_ulong addr,
                                       MMUAccessType access_type,
                                       bool maperr, uintptr_t ra);
+
+#endif /* CONFIG_JOVE_HELPERS */
 
 /**
  * cpu_loop_exit_sigbus:
