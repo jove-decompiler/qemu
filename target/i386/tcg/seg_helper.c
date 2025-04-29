b9b626f1443cc251e9b02f32a7873b2500c9cb03
Anthony Eden <anthony.louis.eden@gmail.com>
2025-04-29 14:24:26 -0700

    [JOVE] undo creation of helper_load_seg() trapping stub for helpers

--- target/i386/tcg/seg_helper.c
+++ target/i386/tcg/seg_helper.c
@@ -1396,16 +1396,6 @@ void helper_ltr(CPUX86State *env, int selector)
 
 #endif
 
-#ifdef CONFIG_JOVE_HELPERS
-
-void helper_load_seg(CPUX86State *env, int seg_reg, int selector)
-{
-    __builtin_trap();
-    __builtin_unreachable();
-}
-
-#else
-
 /* only works if protected mode and not VM86. seg_reg must be != R_CS */
 void helper_load_seg(CPUX86State *env, int seg_reg, int selector)
 {
@@ -1493,9 +1483,6 @@ void helper_load_seg(CPUX86State *env, int seg_reg, int selector)
 #endif
     }
 }
-
-#endif
-
 
 #ifdef CONFIG_JOVE_HELPERS
 
