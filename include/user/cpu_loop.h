aaa15a61f8ae120a9f557b0cd08a70e21440c26c
Anthony Eden <anthony.louis.eden@gmail.com>
2025-05-20 09:04:02 -0700

    [JOVE] vaddr instead of target_ulong

--- include/user/cpu_loop.h
+++ include/user/cpu_loop.h
@@ -49,7 +49,7 @@ bool handle_sigsegv_accerr_write(CPUState *cpu, sigset_t *old_set,
 #ifdef CONFIG_JOVE_HELPERS
 
 inline
-G_NORETURN void cpu_loop_exit_sigsegv(CPUState *cpu, target_ulong addr,
+G_NORETURN void cpu_loop_exit_sigsegv(CPUState *cpu, vaddr addr,
                                       MMUAccessType access_type,
                                       bool maperr, uintptr_t ra) {
     __builtin_trap();
